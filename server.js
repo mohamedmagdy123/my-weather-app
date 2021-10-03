b3994927df4639b8ffcc3adbb5df2b4ef73cc9b5
MohamedMagdy <mohamedmagdy7770@gmail.com>
2021-10-03 22:52:14 +0200

    made routes to get data from api

--- server.js
+++ server.js
@@ -6,15 +6,35 @@ const express = require("express");
 // Start up an instance of app
 const app = express();
 /* Middleware*/
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-const cors = require('cors');
+const cors = require("cors");
+app.use(cors);
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static("website"));
 // Setup Server
-const port = 3000;
-const server = app.listen(port, () => { console.log(port); });
+const port = 5500;
+
+app.get("/all", function (req, res) {
+  res.send("welcome to the server");
+  res.send(projectData);
+});
+const server = app.listen(port, () => {
+  console.log(port);
+});
+
+// app.post("/", addData);
+
+// function addData(req, res) {
+//   newEntry = {
+//     Temprature: req.body.temprature,
+//     data: req.body.date,
+//     feelings: req.body.feelings,
+//   };
+//   projectData.push(newEntry);
+//   res.send(projectData);
+// }
--- website/app.js
+++ website/app.js
@@ -1,5 +1,54 @@
 /* Global Variables */
-
+const myURL = "http://api.openweathermap.org/data/2.5/weather?zip=";
+const key = ",us&appid=4b4a59a39a11d061c8956e9f48f72dfd";
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+
+const generate = document.getElementById("generate");
+console.log("1");
+generate.addEventListener(
+  "click",
+  () => {
+    const ZIP = document.getElementById("zip").value;
+    const feelings = document.getElementById("feelings").value;
+    console.log(`${myURL}${ZIP}${key}`);
+    getData(myURL, ZIP, key).then(function (data) {
+      console.log(data);
+      postData("/all", {
+        temprature: data.main.temp,
+        date: d,
+        userResonse: feelings,
+      });
+    });
+  },
+  false
+);
+const getData = async (URL, ZIP, key) => {
+  console.log(ZIP);
+  const res = await fetch(URL + ZIP + key);
+  try {
+    const data = await res.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+const postData = async (url = "", data = {}) => {
+  const response = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  try {
+    const newData = await response.json();
+    return newData;
+  } catch (error) {
+    console.log("error", error);
+  }
+};
